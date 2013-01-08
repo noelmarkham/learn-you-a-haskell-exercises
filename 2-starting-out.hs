1db318d5d82f5c19098965e564bb8568ab7a5bb8
Noel Markham <noel@mindcandy.com>
2013-01-08 14:30:13 +0000

    Using undefined rather than 0 for functions to complete

--- 2-starting-out.hs
+++ 2-starting-out.hs
@@ -3,7 +3,7 @@
  -
  -You can load (and reload) this file in the interpreter with the command: ":l 2-starting-out.hs"
  -
- -The first function has been completed as an example.All the other functions currently return zero.
+ -The first function has been completed as an example. All the other functions are undefined.
  -They can be implemented in one line using the material covered in http://learnyouahaskell.com/starting-out
  -
  -All indices are zero based.
@@ -13,10 +13,10 @@
 penultimate l = last (init l)
 
 -- Find the kth element in list l
-findK k l = 0
+findK k l = undefined
 
 -- Determine if list l is a palindrome
-isPalindrome l = 0
+isPalindrome l = undefined
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
@@ -25,25 +25,25 @@ isPalindrome l = 0
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = 0
+duplicate xs = undefined
 
 {-
  - Imitate the functinality of zip
  - The function "min x y" returns the lower of values x and y
  -}
-ziplike xs ys = 0
+ziplike xs ys = undefined
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
-splitAtIndex k l = 0
+splitAtIndex k l = undefined
 
 -- Drop the kth element in list l
-dropK k l = 0
+dropK k l = undefined
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
-slice i k l = 0
+slice i k l = undefined
 
 -- Insert element x in list l at index k
-insertElem x k l = 0
+insertElem x k l = undefined
 
 -- Rotate list l n places
-rotate n l = 0
+rotate n l = undefined
