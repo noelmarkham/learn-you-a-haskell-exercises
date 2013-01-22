0715ea57febb78839d9996e9867d8b5e5b43f2bc
Noel Markham <noel@mindcandy.com>
2013-01-22 09:23:07 +0000

    Exercises for function syntax and recursion chapters

---
+++ 4-syntax-in-functions.hs
@@ -0,0 +1,12 @@
+-- This function should print a single digit number as English text, or "unknown" if it's out of the range 0-9
+englishDigit :: Int -> String
+englishDigit x = undefined
+
+-- given a tuple, divide fst by snd, using pattern matching. 
+-- it should return undefined for division by zero
+divTuple :: (Eq a, Fractional a) => (a, a) -> a
+divTuple (x, y) = undefined
+
+-- if the first three numbers in a list are all zero, return True
+threeZeroList :: [Int] -> Bool
+threeZeroList [x] = undefined
