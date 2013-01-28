d811bea5fc6f9b01738ea30ee7827effcbf76373
Noel Markham <noel@mindcandy.com>
2013-01-28 14:41:23 +0000

    Clarification on first question, using linear conversions in an area conversion question

--- 7-modules.hs
+++ 7-modules.hs
@@ -1,9 +1,10 @@
--- First, complete the DistanceConversions.hs module, and import it here
 -- create a higher-order function for converting an area between two dimensions
+-- this will take the function for converting a distance, and an area to convert
 -- using the functions defined in the DistanceConversions module
--- Example areaConv inchesToCentimetres 9 = 58.0644 
+-- Example areaConv inchesToCentimetres 9 = 58.0644
+
 areaConv :: (Float -> Float) -> Float -> Float
-areaConv f x = undefined
+areaConv linearConversion area = undefined
 
 -- define a function for converting square inches into square centimetres
 sqInToSqCm :: Float -> Float
