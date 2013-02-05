4b3abeeab2c0d1d943aa07df5a99365677fc6e48
Noel Markham <noel@mindcandy.com>
2013-02-05 14:18:08 +0000

    Explicit explanation to fill in DistanceConversions

--- 7-modules.hs
+++ 7-modules.hs
@@ -1,3 +1,4 @@
+-- Fill in the DistanceConversions module first, and import it here
 -- create a higher-order function for converting an area between two dimensions
 -- this will take the function for converting a distance, and an area to convert
 -- using the functions defined in the DistanceConversions module
