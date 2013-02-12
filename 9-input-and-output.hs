ed831bddf0b8b1702e2c1e19f51260b1ab1b1f64
Noel Markham <noel@mindcandy.com>
2013-02-12 13:54:48 +0000

    Solutions for chapter 9

--- 9-input-and-output.hs
+++ 9-input-and-output.hs
@@ -1,13 +1,22 @@
+import System.Environment
+import Data.List
+import System.Random
+
 {-
  - Lets implement the UNIX echo command
  - The program arguments are simply printed to the standard output.
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
-  
-main = undefined
+main = do
+   first:args <- getArgs
+   let noNewLineSwitch = first == "-n"
+   let termination = if noNewLineSwitch then putStr else putStrLn
+   let argsToPrint = if noNewLineSwitch then args else first:args
+   mapM putStr $ intersperse " " argsToPrint
+   termination ""
 
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
  -}
 lottery :: StdGen -> [Int]
-lottery gen = undefined
+lottery gen = sort $ take 6 $ nub $ randomRs (1, 49) gen
