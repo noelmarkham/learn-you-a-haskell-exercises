e86c85d914806a7803a733d364d969da6c638cde
Katherine Ye <kye@princeton.edu>
2013-06-06 15:03:59 -0300

    Update 5-recursion.hs
    solutions for power function missing

--- 5-recursion.hs
+++ 5-recursion.hs
@@ -1,7 +1,14 @@
 -- Raise x to the power y, using recursion
 -- For example, power 5 2 = 25
 power :: Int -> Int -> Int
-power x y = undefined
+power _ 0 = 1
+power x y = x * power x (y - 1)
+
+power :: (Eq a, Ord a, Fractional a) => a -> a -> a
+power x y 
+        | y < 0 = power (1 / x) (-y)
+ 	| y == 0 = 1
+ 	| otherwise = x * power x (y - 1)
 
 -- create a list of length n of the fibbonaci sequence in reverse order
 -- examples: fib 0 = [0]
