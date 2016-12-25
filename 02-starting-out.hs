bb85f3f367bafe3dd3c3145364a5f8f640c0c604
Abraham Neuwirth <Yeedle@users.noreply.github.com>
2016-12-25 12:25:25 -0500

    clarified line 33

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -30,7 +30,7 @@ duplicate xs = undefined
 
 {-
  - Imitate the functinality of zip
- - The function "min x y" returns the lower of values x and y
+ - hint The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
 ziplike xs ys = undefined
